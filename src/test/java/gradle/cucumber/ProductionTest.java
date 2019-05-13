a585ed852199d0f5351ab11cd3d46abcd020aa96
Brian Craig <briancraigok@gmail.com>
2019-05-13 04:40:48 -0300

    try CI #9

---
+++ src/test/java/gradle/cucumber/ProductionTest.java
@@ -0,0 +1,14 @@
+package gradle.cucumber;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductionTest {
+
+    @Test
+    void doWork() {
+        assertTrue(true);
+    }
+}
