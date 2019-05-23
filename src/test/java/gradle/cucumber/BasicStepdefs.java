7b781a716ef8719b0e06487178e76205006433ed
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2019-05-22 22:20:44 -0300

    se agrega el archivo de travis y al ejecutar gradle check se corre el proyecto

--- src/test/java/gradle/cucumber/BasicStepdefs.java
+++ src/test/java/gradle/cucumber/BasicStepdefs.java
@@ -1,11 +1,19 @@
 package gradle.cucumber;
 
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class BasicStepdefs {
 
     @When("^I run a failing step")
     public void I_run_a_failing_step() throws Throwable {
         new Production().doWork();
+    }
+    @Then( "all Run" )
+    public void all_run(){
+        assertTrue(true);
     }
 }
