6073341c99de1b84ffcd79c97b0152ab4042363b
Brian Craig <briancraigok@gmail.com>
2019-05-26 01:02:27 -0300

    Remove unused enemy isAlive logic

--- src/main/java/bomberman/Enemy.java
+++ src/main/java/bomberman/Enemy.java
@@ -2,8 +2,6 @@ package bomberman;
 
 public class Enemy implements CellEntity {
 
-    private Boolean alive = true ;
-
     @Override
     public boolean blocksMovement() {
         return false;
@@ -14,19 +12,10 @@ public class Enemy implements CellEntity {
         if(Bomberman.class.isAssignableFrom(anotherEntity.getClass())) {
             ((Bomberman)anotherEntity).kill();
         }
-        if(Bomb.class.isAssignableFrom((anotherEntity.getClass()))){
-            this.kill();
-        }
     }
 
     @Override
     public void burnFromExplosion(Cell cell) {
         cell.remove(this);
-    }
-
-    public boolean isAlive (){return alive;}
-
-    public void kill() {
-        this.alive = false;
     }
 }
